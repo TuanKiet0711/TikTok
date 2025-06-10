8f90038de30f63cfc302b1cbbfc2483604e367f2
kitlee0 <1150080022@sv.hcmunre.edu.vn>
2025-06-10 12:38:25 +0700

    up logo

--- src/components/Layout/DefaultLayout/Header/index.js
+++ src/components/Layout/DefaultLayout/Header/index.js
@@ -1,11 +1,12 @@
 import style from '~/components/Layout/DefaultLayout/Header/Header.module.scss';
 import classNames from 'classnames/bind';
+import images from '~/assets/images';
 const cx = classNames.bind(style);
 function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                Header
+                <img src={images.logo} className={cx('logo')} alt="TikTok" />
             </div>
         </header>
     );
