5b2c41d408c556a56e909b26a6dfd4f0cd5ae66c
kitlee0 <1150080022@sv.hcmunre.edu.vn>
2025-06-13 16:20:13 +0700

    search and clear

--- src/components/Layout/DefaultLayout/Header/index.js
+++ src/components/Layout/DefaultLayout/Header/index.js
@@ -1,12 +1,31 @@
 import style from '~/components/Layout/DefaultLayout/Header/Header.module.scss';
 import classNames from 'classnames/bind';
 import images from '~/assets/images';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 const cx = classNames.bind(style);
 function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                <img src={images.logo} className={cx('logo')} alt="TikTok" />
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="TikTok" />
+                </div>
+                <div className={cx('search')}>
+                    <input placeholder="Search accounts and videos" spellCheck="false" />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('actions')}>
+                    <button className={cx('upload-btn')}>Upload</button>
+                    <button className={cx('message-btn')}>Message</button>
+                    <button className={cx('inbox-btn')}>Inbox</button>
+                    <button className={cx('profile-btn')}>Profile</button>
+                </div>
             </div>
         </header>
     );
